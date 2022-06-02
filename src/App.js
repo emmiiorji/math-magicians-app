2d6d485bbaf7de2284c61b7a9551ad6b949bd941
emmiiorji <emmanuelorji2013@gmail.com>
2022-06-02 19:44:28 +0100

    Refactor App.js to be class-based

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import React from 'react';
 import Calculator from './components/Calculator';
 
-function App() {
-  return <Calculator />;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+  }
+
+  render() {
+    return <Calculator />;
+  }
 }
 
 export default App;
