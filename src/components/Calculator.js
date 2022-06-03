a5f0af332f4385af21d18f8163ee2e132bf1b667
emmiiorji <emmanuelorji2013@gmail.com>
2022-06-03 19:38:50 +0100

    Refactor and event to the Calculator component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Display from './Display';
 import Keypad from './Keypad';
+import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
+class Calculator extends Component {
   constructor(props) {
     super(props);
     this.id = props.id;
+    this.handleKeyClick = this.handleKeyClick.bind(this);
+    this.state = {
+      total: 0,
+      next: null,
+      operation: null,
+    };
+  }
+
+  handleKeyClick(buttonName) {
+    this.setState((state) => {
+      const result = calculate(state, buttonName);
+      return result;
+    });
   }
 
   render() {
+    const { next, operation } = this.state;
+    let { total } = this.state;
+    total = total === null && next ? '' : total;
+
     return (
       <div id={this.id}>
-        <Display />
-        <Keypad />
+        <Display total={total === null ? 0 : total} next={next || ''} operation={operation || ''} />
+        <Keypad handleKeyClick={this.handleKeyClick} />
       </div>
     );
   }
