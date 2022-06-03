252ed341ec00bc3d281e6db24b61282bb6b85458
emmiiorji <emmanuelorji2013@gmail.com>
2022-06-03 19:38:11 +0100

    Refactor and event to the Display component

--- src/components/Display.js
+++ src/components/Display.js
@@ -4,22 +4,31 @@ import React from 'react';
 class Display extends React.Component {
   constructor(props) {
     super(props);
-    [this.id, this.value] = [props.id, props.value];
+    [this.id, this.total] = [props.id, props.total];
   }
 
   render() {
-    return <div id={this.id}>{this.value}</div>;
+    const { total, operation, next } = this.props;
+    return (
+      <div id={this.id}>
+        {`${total}${operation}${next}`}
+      </div>
+    );
   }
 }
 
 Display.defaultProps = {
   id: 'display',
-  value: 0,
 };
 
 Display.propTypes = {
   id: PropTypes.string,
-  value: PropTypes.number,
+  total: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ]).isRequired,
+  operation: PropTypes.string.isRequired,
+  next: PropTypes.string.isRequired,
 };
 
 export default Display;
