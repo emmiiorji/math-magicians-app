15dbb93d9cf79fa8f7d7722ca7cf69e0fba1592b
emmiiorji <emmanuelorji2013@gmail.com>
2022-06-03 19:37:23 +0100

    Add event to the Key component

--- src/components/Key.js
+++ src/components/Key.js
@@ -5,11 +5,17 @@ class Key extends React.Component {
   constructor(props) {
     super(props);
     [this.value, this.className, this.id] = [props.value, props.className, props.id];
+    this.handleKeyClick = this.handleKeyClick.bind(this);
+  }
+
+  handleKeyClick(e) {
+    const { handleKeyClick } = this.props;
+    handleKeyClick(e.target.innerText);
   }
 
   render() {
     return (
-      <button type="button" className={this.className} id={this.id}>
+      <button type="button" className={this.className} id={this.id} onClick={this.handleKeyClick}>
         {this.value}
       </button>
     );
@@ -25,6 +31,7 @@ Key.propTypes = {
   value: PropTypes.string.isRequired,
   className: PropTypes.string,
   id: PropTypes.string,
+  handleKeyClick: PropTypes.func.isRequired,
 };
 
 export default Key;
