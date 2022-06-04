566e4e172ab0cceaab4f9bc402616b9f2dfce629
emmiiorji <emmanuelorji2013@gmail.com>
2022-06-04 07:12:26 +0100

    Refactor Calculator component to a class component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,52 +1,38 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+
 import Display from './Display';
 import Keypad from './Keypad';
 import calculate from '../logic/calculate';
 
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.id = props.id;
-    this.handleKeyClick = this.handleKeyClick.bind(this);
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
-  }
+const Calculator = ({ id }) => {
+  const [calcObject, setCalcObject] = useState({ total: 0, next: null, operation: null });
 
-  handleKeyClick(buttonName) {
-    this.setState((state) => {
+  const handleKeyClick = (buttonName) => {
+    setCalcObject((state) => {
       const result = calculate(state, buttonName);
       return result;
     });
-  }
+  };
 
-  render() {
-    const { next, operation } = this.state;
-    let { total } = this.state;
-    total = total === null && next ? '' : total;
+  const { next, operation } = calcObject;
+  let { total } = calcObject;
+  total = total === null && next !== null ? '' : total;
 
-    return (
-      <div id={this.id}>
-        <Display
-          total={total === null ? 0 : total}
-          next={next || ''}
-          operation={operation || ''}
-        />
-        <Keypad handleKeyClick={this.handleKeyClick} id="keypad" />
-      </div>
-    );
-  }
-}
-
-Calculator.defaultProps = {
-  id: 'calculator',
+  return (
+    <div id={id}>
+      <Display
+        total={total === null ? 0 : total}
+        next={next || ''}
+        operation={operation || ''}
+      />
+      <Keypad handleKeyClick={handleKeyClick} id="keypad" />
+    </div>
+  );
 };
 
 Calculator.propTypes = {
-  id: PropTypes.string,
+  id: PropTypes.string.isRequired,
 };
 
 export default Calculator;
