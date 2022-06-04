d22006d9fb890aefc24b5f65dee2c681dd46d802
emmiiorji <emmanuelorji2013@gmail.com>
2022-06-04 07:16:39 +0100

    Refactor Keypad component to a class component

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,59 +1,38 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+
 import Key from './Key';
 
-class Keypad extends React.Component {
-  constructor(props) {
-    super(props);
-    this.id = props.id;
-  }
-
-  render() {
-    const { handleKeyClick } = this.props;
-    const symbols = [
-      { value: 'AC', id: 'AC' }, { value: '+/-', id: 'plus-minus' },
-      { value: '%', id: 'percent' }, { value: '÷', id: 'divide', className: 'key orange' },
-      { value: '7', id: 'seven' }, { value: '8', id: 'eight' },
-      { value: '9', id: 'nine' }, { className: 'key orange', value: 'x', id: 'times' },
-      { value: '4', id: 'four' }, { value: '5', id: 'five' },
-      { value: '6', id: 'six' }, { className: 'key orange', value: '-', id: 'minus' },
-      { value: '1', id: 'one' }, { value: '2', id: 'two' },
-      { value: '3', id: 'three' }, { value: '+', id: 'plus', className: 'key orange' },
-      { value: '0', id: 'zero' }, { value: '.', id: 'dot' }, { value: '=', id: 'equal', className: 'key orange' },
-    ];
-    return (
-      <div id={this.id}>
-        {symbols.map((symbol) => (
-          symbol.className
-            ? (
-              <Key
-                value={symbol.value}
-                id={symbol.id}
-                key={`${symbol.id}2`}
-                className={symbol.className}
-                handleKeyClick={handleKeyClick}
-              />
-            )
-            : (
-              <Key
-                value={symbol.value}
-                id={symbol.id}
-                key={`${symbol.id}1`}
-                handleKeyClick={handleKeyClick}
-              />
-            )
-        ))}
-      </div>
-    );
-  }
-}
-
-Keypad.defaultProps = {
-  id: 'keypad',
+const Keypad = ({ id, handleKeyClick }) => {
+  const symbols = [
+    { value: 'AC', id: 'AC', className: 'key' }, { value: '+/-', id: 'plus-minus', className: 'key' },
+    { value: '%', id: 'percent', className: 'key' }, { value: '÷', id: 'divide', className: 'key orange' },
+    { value: '7', id: 'seven', className: 'key' }, { value: '8', id: 'eight', className: 'key' },
+    { value: '9', id: 'nine', className: 'key' }, { value: 'x', id: 'times', className: 'key orange' },
+    { value: '4', id: 'four', className: 'key' }, { value: '5', id: 'five', className: 'key' },
+    { value: '6', id: 'six', className: 'key' }, { value: '-', id: 'minus', className: 'key orange' },
+    { value: '1', id: 'one', className: 'key' }, { value: '2', id: 'two', className: 'key' },
+    { value: '3', id: 'three', className: 'key' }, { value: '+', id: 'plus', className: 'key orange' },
+    { value: '0', id: 'zero', className: 'key' }, { value: '.', id: 'dot', className: 'key' },
+    { value: '=', id: 'equal', className: 'key orange' },
+  ];
+  return (
+    <div id={id}>
+      {symbols.map((symbol) => (
+        <Key
+          value={symbol.value}
+          id={symbol.id}
+          key={`${symbol.id}2`}
+          className={symbol.className}
+          onClick={handleKeyClick}
+        />
+      ))}
+    </div>
+  );
 };
 
 Keypad.propTypes = {
-  id: PropTypes.string,
+  id: PropTypes.string.isRequired,
   handleKeyClick: PropTypes.func.isRequired,
 };
 
